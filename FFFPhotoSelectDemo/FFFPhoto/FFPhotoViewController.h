396f55d817bc6e0eb5773faf3f3c190d105ab48d
殷玉秋 <yinyuqiu@yinyuqiudeMacBook-Air.local>
2017-05-12 14:36:40 +0800

    bug fix

--- FFFPhotoSelectDemo/FFFPhoto/FFPhotoViewController.h
+++ FFFPhotoSelectDemo/FFFPhoto/FFPhotoViewController.h
@@ -3,7 +3,6 @@
 //  PFCS
 //
 //  Created by flappybird on 16/8/4.
-//  Copyright © 2016年 PFCS. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- FFFPhotoSelectDemo/FFFPhotoSelectDemo/ViewController.m
+++ FFFPhotoSelectDemo/FFFPhotoSelectDemo/ViewController.m
@@ -2,8 +2,6 @@
 //  ViewController.m
 //  FFFPhotoSelectDemo
 //
-//  Created by 殷玉秋 on 2017/5/8.
-//  Copyright © 2017年 殷玉秋. All rights reserved.
 //
 
 #import "ViewController.h"
